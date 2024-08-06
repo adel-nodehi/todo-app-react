b56395592a685f8b18a96b90d4c25a4cf5ed4bb5
Adel <adelnmo2003@gmail.com>
2024-08-06 23:51:28 +0330

    add todoSlice to store

--- src/store.js
+++ src/store.js
@@ -1,7 +1,9 @@
 import { configureStore } from "@reduxjs/toolkit";
 
+import todoSlice from "./components/todoSlice";
+
 const store = configureStore({
-  reducer: {},
+  reducer: { todo: todoSlice },
 });
 
 export { store };
